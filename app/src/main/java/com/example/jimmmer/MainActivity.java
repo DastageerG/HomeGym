4b6f473a7edadcbce9446702274055a9a952c0f0
ghulam dastageer <dastageerg44@gmail.com>
2020-07-28 11:10:48 +0500

    design changes

--- app/src/main/java/com/example/jimmmer/MainActivity.java
+++ app/src/main/java/com/example/jimmmer/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.jimmmer.adapter.AdapterForWorkoutList;
+import com.example.jimmmer.applicationClass.MyApplicationClass;
 import com.example.jimmmer.model.WorkOutBean;
 
 import java.util.ArrayList;
@@ -19,45 +21,18 @@ public class MainActivity extends AppCompatActivity
 {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    private List<WorkOutBean>exercisesChosenList;
-    private int random_exercise;
-    private List<Integer> listOfNumbersGenerated = new ArrayList();
-    private WorkOutBean[] totalExercisesArray =
-            {
-                    new WorkOutBean("Bench Press",R.drawable.bench_press,3) ,
-                    new WorkOutBean("Bicep Curl",R.drawable.bicep_curl,3) ,
-                    new WorkOutBean("Chest Press",R.drawable.chest_press,2) ,
-                    new WorkOutBean("Crunches",R.drawable.crunches,3) ,
-                    new WorkOutBean("Forward Bend",R.drawable.forward_bend,1) ,
-                    new WorkOutBean("Incline Chest Press",R.drawable.incline_chest_press,2) ,
-                    new WorkOutBean("Push Ups",R.drawable.push_up,1) ,
-                    new WorkOutBean("Renegade Row",R.drawable.renegade_row,4) ,
-                    new WorkOutBean("Seated Cable Row",R.drawable.seated_cable_row,3) ,
-                    new WorkOutBean("Side Stretch",R.drawable.side_strech,2) ,
-                    new WorkOutBean("Sit Ups",R.drawable.situps,3) ,
-                    new WorkOutBean("Swiss Ball Push Ups",R.drawable.swiss_ball_push_up,2) ,
-                    new WorkOutBean("Tree Pose",R.drawable.tree_pose,1) ,
-                    new WorkOutBean("Vertical Leg Crunch",R.drawable.vertical_leg_crunch,2)
-            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //
-        exercisesChosenList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycleViewMainActivity);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        Random random = new Random();
 
-        for(int i = 0;i<5;i++)
-        {
-            random_exercise = random.nextInt(13); // is of Int type
-            listOfNumbersGenerated.add(random_exercise);
-            exercisesChosenList.add(totalExercisesArray[random_exercise]);
-        } //
 
-        adapter = new AdapterForWorkoutList(this,exercisesChosenList);
+        adapter = new AdapterForWorkoutList(this, MyApplicationClass.exercisesChosenList);
         recyclerView.setAdapter(adapter);
     }
 }
